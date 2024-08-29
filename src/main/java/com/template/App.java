b23e8c5fb5c9afb9bfaa5d222416295abc55709c
Máximo Zúñiga <mjzunigaricardi@mail.austral.edu.ar>
2024-08-29 16:26:08 -0400

    TP1_JAVA_MAXIMOZUNIGA

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -4,61 +4,72 @@ public class App {
 
     // 1. Sum of Two Integers
     public int sum(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        return a + b;
     }
 
     // 2. Check Even or Odd
     public boolean isEven(int number) {
-        // TODO: Implement this method
+        if (number%2==0){return true;}
         return false;
     }
 
     // 3. Maximum of Two Numbers
     public int max(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        if (a>b){return a;}
+        return b;
     }
 
     // 4. Factorial of a Number
     public int factorial(int n) {
-        // TODO: Implement this method
-        return 0;
+        int factorial=1;
+        for(int i = 1; i <=n; i++) {
+            factorial*=i;
+
+        }
+        return factorial;
     }
 
     // 5. Count Characters in a String
     public int countChars(String input) {
-        // TODO: Implement this method
-        return 0;
+
+        return input.length();
     }
 
     // 6. Reverse a String
     public String reverse(String input) {
-        // TODO: Implement this method
-        return "";
+        String new_word="";
+
+        for(int i = 1; i<=input.length(); i++){new_word+=input.charAt(input.length()-i);}
+        return new_word;
     }
 
     // 7. Check Prime Number
     public boolean isPrime(int number) {
-        // TODO: Implement this method
+        Integer answer = 0;
+        for(int i = 1; i < number; i++){if(number%i==0){answer+=i;}}
+        if (answer == 1){return true;}
         return false;
     }
 
     // 8. Find the Smallest Element in an Array
     public int findMin(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        int value=array[0];
+        for(int i = 0; i < array.length; i++){
+            if (value>=array[i]){ value=array[i];}
+        }
+        return value;
     }
 
     // 9. Sum of Elements in an Array
     public int arraySum(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        Integer suma =0;
+        for(int i = 0; i < array.length; i++){ suma += array[i];}
+        return suma;
     }
 
     // 10. Convert Celsius to Fahrenheit
     public double celsiusToFahrenheit(double celsius) {
-        // TODO: Implement this method
-        return 0.0;
+        double fahrenheit= ((9 * celsius)/5) + 32;
+        return fahrenheit;
     }
 }
